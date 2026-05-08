f457cfb06fa27e666d38527caaa21850585fbf06
Ben Hampton <benhampton11@yahoo.com>
2026-05-07 22:02:22 -0500

    init spring-boot project

---
+++ spring-boot/api-okta-demo-service/src/main/java/com/api_okta_demo_service/ApiOktaDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api_okta_demo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOktaDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOktaDemoServiceApplication.class, args);
+	}
+
+}
---
+++ spring-boot/api-okta-demo-service/src/test/java/com/api_okta_demo_service/ApiOktaDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_okta_demo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOktaDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
